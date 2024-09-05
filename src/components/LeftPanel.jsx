521a9bca62a657971b4e24c58085d7cee1716da1
abhichandra03 <abhichandra03@gmail.com>
2024-09-05 14:35:36 +0530

    alignment

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LeftPanel = () => {
   return (
-    <div className='w-full h-[100px] lg:w-1/2 lg:h-[720px] max-w-[720px] mr-8 2xl:mr-0'>
+    <div className='w-full h-[100px] lg:w-1/2 flex justify-center lg:items-start items-center lg:h-[720px] max-w-[720px] mr-0 lg:mr-8 mb-8 lg:mb-0'>
         <div className='w-full h-full max-h-[690px] bg-[#616161D1] border-[#96BEE7] border-[1px] rounded-lg'>
 
         </div>
