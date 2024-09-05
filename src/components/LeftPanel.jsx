9391f838cd793e89faf616b7f25fcd13d58cd646
abhichandra03 <abhichandra03@gmail.com>
2024-09-05 14:14:48 +0530

    completed

---
+++ src/components/LeftPanel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LeftPanel = () => {
+  return (
+    <div className='w-full h-[100px] lg:w-1/2 lg:h-[720px] max-w-[720px] lg:mr-8'>
+        <div className='w-full h-full max-h-[690px] bg-[#616161D1] border-[#96BEE7] border-[1px] rounded-lg'>
+
+        </div>
+    </div>
+  )
+}
+
+export default LeftPanel
