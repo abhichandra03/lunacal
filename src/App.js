78f45d3be63e46df7d0ca6640316c00c779428e3
abhichandra03 <abhichandra03@gmail.com>
2024-09-05 14:21:43 +0530

    alignment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import RightPanel from './components/RightPanel';
 
 function App() {
   return (
-    <div className="flex flex-col lg:flex-row w-full h-screen justify-evenly bg-red-100 lg:justify-around items-center  bg-gradient-to-r from-[#373E44] to-[#191B1F] shadow-[10px_10px_40px_10px_rgba(0,0,0,0.5)] overflow-hidden lg:p-10">
+    <div className="flex flex-col lg:flex-row w-full h-screen justify-center lg:justify-evenly items-center  bg-gradient-to-r from-[#373E44] to-[#191B1F] shadow-[10px_10px_40px_10px_rgba(0,0,0,0.5)] overflow-hidden lg:p-10">
       <LeftPanel/>
       <RightPanel/>
     </div>
--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LeftPanel = () => {
   return (
-    <div className='w-full h-[100px] lg:w-1/2 lg:h-[720px] max-w-[720px] lg:mr-8'>
+    <div className='w-full h-[100px] lg:w-1/2 lg:h-[720px] max-w-[720px] mr-8 2xl:mr-0'>
         <div className='w-full h-full max-h-[690px] bg-[#616161D1] border-[#96BEE7] border-[1px] rounded-lg'>
 
         </div>
